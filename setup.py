815da01da316632911867d5e112687fffb675b04
w3schools-test <kgopinath730@gmail.com>
2025-06-04 11:34:58 +0530

    setuP.py complete

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup,find_packages
+from typing import List 
+
+HYPEN_E_DOT = "-e ."
+
+def getrequirements()-> List[str]:
+    """
+    Returns:
+        List[str]: _description_ 
+    """
+    requirements=[]
+
+    try:
+        with open("requirements.txt", 'r') as f:
+            requirements = f.readlines()
+            requirements = [req.strip("\n") for req in requirements]
+            if HYPEN_E_DOT in requirements:
+                requirements.remove(HYPEN_E_DOT)
+            return requirements
+    except Exception as E:
+        print(E)
+
+
+setup(
+    name="networksecurity",
+    description="this is my second project following krish naik's ml course ",
+    author_email="kgopinath730@gmail.com",
+    packages=find_packages(),
+    install_requires=getrequirements()
+
+)
